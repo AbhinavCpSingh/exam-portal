5a9c1385bb78d78856009992e76d03ad227e5c74
Abhinav Singh <85756353+AbhinavCpSingh@users.noreply.github.com>
2025-11-21 15:21:33 +0530

    Added Dockerfile for Jenkins pipeline

--- exam_portal/exam_portal/settings.py
+++ exam_portal/exam_portal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY") or os.urandom(50).hex()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.226.33.60',] 
+ALLOWED_HOSTS = ['10.226.33.60','*',] 
 
 # Application definition 
 
